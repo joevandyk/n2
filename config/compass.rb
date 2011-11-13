98c51a4cf6b312317f8365266e29770a3d600916
Joe Van Dyk <joe@fixieconsulting.com>
2011-11-12 16:50:07 -0800

    Moved stylesheets from app/stylesheets to app/assets/stylesheets

--- config/compass.rb
+++ config/compass.rb
@@ -1,15 +1,13 @@
 require 'ninesixty'
-# Require any additional compass plugins here.
 
 project_type = :rails
 project_path = Rails.root if defined?(Rails.root)
-# Set this to the root of your project when deployed:
+
 http_path = "/"
 css_dir = "public/stylesheets/compiled"
-sass_dir = "app/stylesheets"
+sass_dir = "app/assets/stylesheets"
 images_dir = "app/assets/images"
 javascripts_dir = "app/assets/javascripts"
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+
 http_stylesheets_path = "/assets"
 http_javascripts_path = "/assets"
