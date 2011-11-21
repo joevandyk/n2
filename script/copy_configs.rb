c9c9fe0e7fa3825a885ca7f6ef951431f8c291bf
Joe Van Dyk <joe@fixieconsulting.com>
2011-11-21 14:17:22 -0800

    update travis

--- script/copy_configs.rb
+++ script/copy_configs.rb
@@ -1,7 +1,14 @@
 #!/usr/bin/env ruby
 
-Dir["config/\*.yml.sample"].each do |file|
-  command = "cp #{ file } #{ file.sub('.sample', '')}"
+files = {
+  "resque_schedule.yml.sample" => "resque_schedule.yml",
+  "resque.yml.sample"          => "resque.yml",
+  "database.ci.yml"            => "database.yml",
+  "application_settings.yml.sample" => "application_settings.yml"
+}
+
+files.each do |orig, dest|
+  command = "cp config/#{orig} config/#{dest}"
   puts command
   `#{ command }`
 end
