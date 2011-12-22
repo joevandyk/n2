bbda28d8276c511b09470b7a4ce90a8adaca104e
Joe Van Dyk <joe@fixieconsulting.com>
2011-12-21 23:42:15 -0800

    Site model shouldn't be scoped to the current site

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,12 +1,16 @@
 class Site < ActiveRecord::Base
-  include N2::CurrentSite
   validates :domain, :name, :presence => true
 
   def self.current
-    @current_site
+    @current_site || Site.default
   end
 
   def self.current_domain= domain
     @current_site = Site.where('lower(domain) = ?', domain.downcase).first
+  end
+
+  # There should always be a default site (right?)
+  def self.default
+    Site.order('id').first
   end
 end
