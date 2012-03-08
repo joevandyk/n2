7d5d1d00f540eca502ffda0e622a394aaf2868fa
Joe Van Dyk <joe@fixieconsulting.com>
2012-03-08 08:42:44 -0800

    Added few more things to acceptance tests

--- spec/acceptance/front_page_spec.rb
+++ spec/acceptance/front_page_spec.rb
@@ -4,10 +4,7 @@ describe "Front page" do
   it 'should load' do
     visit "/"
 
-    # Apparently the master branch removed these?
-    # click_on "News"
-    # click_on "Galleries"
-    # click_on "Register"
+    click_on "News"
+    click_on "Register"
   end
-
 end
